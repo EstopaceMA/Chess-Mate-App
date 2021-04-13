f26fcc6445ad4142628eb1d27c99eb0a2d1375d7
MarkAnthony66 <mark.estopace@opsolutions.biz>
2021-04-14 06:00:28 +0800

    Added Initial Welcome Screen and Navigation

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Screen from './src/components/Screen';
 
 import ChessPiece from './src/components/ChessPiece';
 
@@ -15,47 +17,85 @@ const initialPosition = [
   ["p", "p", "p", "p", "p", "p", "p", "p"],
   ["r", "n", "b", "q", "k", "b", "n", "r"]
 ];
+const Stack = createStackNavigator();
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <Screen>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Chessy</Text>
+        <Text>Chessy</Text>
+        <Button
+          title="LETS PLAY"
+          onPress={() => navigation.navigate('Chessy')}
+        />
+      </View>
+    </Screen>
+  );
+}
+
+function ChessboardScreen({ navigation }) {
+  const [position] = useState(initialPosition);
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <Button
+            title=""
+            color="#EC3C3D"
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+        <View style={{ backgroundColor: "#000", flex: 2}}>
+          {position.map((rowOfPieces, rowIndex) => (
+            <View key={rowIndex+'row'} style={{
+              flexDirection: (rowIndex % 2 === 1) ? "row" : "row-reverse",
+              height: "12.5%",
+            }}>
+              {rowOfPieces.map((piece, colIndex) => (
+                <View 
+                  key={rowIndex+'row'+colIndex+'col'} 
+                  style={{
+                    flex: 1, 
+                    flexBasis: "12.5%",
+                    justifyContent: "center", 
+                    backgroundColor: (colIndex % 2 === 1) ? "skyblue" : "steelblue",
+                    alignItems: "center"
+                }}>
+                  {(piece !== "") && <ChessPiece piece={piece} />}
+                </View>
+              ))}
+            </View>
+          ))}
+        </View>
+        <View style={{ flex: 1, justifyContent: "flex-end" }} >
+          <Button
+            title=""
+            color="#49BD78"
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+      </View>
+    </Screen>
+  );
+}
 
 export default function App() {
-  const [position, setPosition] = useState(initialPosition);
-
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1 }} />
-      <View style={{ backgroundColor: "#000", flex: 2}}>
-        {position.map((rowOfPieces, rowIndex) => (
-
-          <View key={rowIndex+'row'} style={{
-            flexDirection: (rowIndex % 2 === 1) ? "row" : "row-reverse",
-            height: "12.5%",
-          }}>
-            {rowOfPieces.map((piece, colIndex) => (
-              <View 
-                key={rowIndex+'row'+colIndex+'col'} 
-                style={{
-                  flex: 1, 
-                  flexBasis: "12.5%",
-                  justifyContent: "center", 
-                  backgroundColor: (colIndex % 2 === 1) ? "skyblue" : "steelblue",
-                  alignItems: "center"
-              }}>
-                {(piece !== "") && <ChessPiece piece={piece} />}
-              </View>
-            ))}
-          </View>
-        ))}
-      </View>
-      <View style={{ flex: 1 }} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Chessy" component={ChessboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: "#1e1e1e",
     flex: 1,
     padding: 10,
-    flexDirection: "column"
   },
 });
 
